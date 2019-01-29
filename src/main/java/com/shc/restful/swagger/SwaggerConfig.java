4489868189fca09e6799b5b4b84d7a99534aacbe
saul <saul.huerta@gmail.com>
2019-01-28 18:51:58 -0600

    Se actualiza path de controllers.

--- src/main/java/com/shc/restful/swagger/SwaggerConfig.java
+++ src/main/java/com/shc/restful/swagger/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.shc.restful.swagger.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
